a961961365db316d5cde734b6e9ed28a760b5cec
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-05-23 23:54:55 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,                                 // Componentes, diretivs e Pipes  
+    MeuPrimeiroComponent, MeuPrimeiro2Component
   ],
-  imports: [
+  imports: [                                      // Outros módulos que queremos utilizar
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],                                  // Colocamos os serviços disponíveis
+  bootstrap: [AppComponent]                       // Qual componente que será instanciado como Default
 })
 export class AppModule { }
---
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro2-component',
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrls: ['./meu-primeiro2.component.css']
+})
+export class MeuPrimeiro2Component  {}
